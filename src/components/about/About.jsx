7be65745d2b7aa2729f45d9dbe83a8e420e3576e
Nurlan <osmonalievnurlan97@gmail.com>
2023-09-16 13:12:39 +0600

    about section changed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -30,7 +30,7 @@ const About = () => {
             <article className='about__card'>
               <VscFolderLibrary className='about__icon' />
               <h5>Projects</h5>
-              <small>5+ completed</small>
+              <small>10+ completed</small>
             </article>
             
             <article className='about__card'>
